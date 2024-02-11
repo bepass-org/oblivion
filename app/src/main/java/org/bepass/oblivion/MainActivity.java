13fdad9dc4d5173d860962e2cf085d0394597cf8
uoosef <ghobadi@yekaan.ir>
2024-02-11 22:01:43 +0330

    improve connect speed

--- app/src/main/java/org/bepass/oblivion/MainActivity.java
+++ app/src/main/java/org/bepass/oblivion/MainActivity.java
@@ -24,6 +24,9 @@ import androidx.core.content.ContextCompat;
 
 import com.suke.widget.SwitchButton;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 import tun2socks.StartOptions;
 
 public class MainActivity extends AppCompatActivity {
@@ -207,14 +210,40 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
+    private static int findFreePort() {
+        ServerSocket socket = null;
+        try {
+            socket = new ServerSocket(0);
+            socket.setReuseAddress(true);
+            int port = socket.getLocalPort();
+            try {
+                socket.close();
+            } catch (IOException e) {
+                // Ignore IOException on close()
+            }
+            return port;
+        } catch (IOException e) {
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+        throw new IllegalStateException("Could not find a free TCP/IP port to start embedded Jetty HTTP Server on");
+    }
+
     private String getBindAddress() {
-        String B = "";
         String port = fileManager.getString("USERSETTING_port");
         boolean enableLan = fileManager.getBoolean("USERSETTING_lan");
+        if(OblivionVpnService.isLocalPortInUse("127.0.0.1:" + port).equals("true")) {
+            port = findFreePort()+"";
+        }
         String Bind = "";
-        Bind += B +  "127.0.0.1:" + port;
+        Bind += "127.0.0.1:" + port;
         if(enableLan) {
-            Bind = B +  "0.0.0.0:" + port;
+            Bind = "0.0.0.0:" + port;
         }
         return Bind;
     }
