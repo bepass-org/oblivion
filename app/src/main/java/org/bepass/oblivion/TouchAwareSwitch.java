14b492b5ccbf4830fb393f42147a7cf31cea33e1
Amillinium <cr.esmaeeili@yahoo.com>
2024-02-12 06:20:02 +0330

    Implement quick settings toggle button

---
+++ app/src/main/java/org/bepass/oblivion/TouchAwareSwitch.java
@@ -0,0 +1,51 @@
+package org.bepass.oblivion;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.suke.widget.SwitchButton;
+
+public class TouchAwareSwitch extends SwitchButton {
+
+    public TouchAwareSwitch(Context context) {
+        super(context);
+    }
+
+    public TouchAwareSwitch(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TouchAwareSwitch(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public void setOnCheckedChangeListener(final OnCheckedChangeListener listener) {
+        setOnTouchListener((v, event) -> {
+            setTag(null);
+            return false;
+        });
+
+        super.setOnCheckedChangeListener((view, isChecked) -> {
+            if (getTag() != null) {
+                setTag(null);
+                return;
+            }
+            listener.onCheckedChanged(view, isChecked);
+        });
+    }
+
+    public void setChecked(boolean checked, boolean notify) {
+        if (!notify) setTag("TAG");
+        setChecked(checked);
+    }
+
+
+
+
+
+
+}
