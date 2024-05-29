2317604071e1573dfc2b2a72ff4b3e4da0321747
DanielcoderX <danielcoderx@keemail.me>
2024-05-29 19:55:58 +0330

    Implement translations and fix issues
    Implement translations and fix issues
    
    Implement translations and fix issues

--- app/src/main/java/org/bepass/oblivion/SplashScreenActivity.java
+++ app/src/main/java/org/bepass/oblivion/SplashScreenActivity.java
@@ -1,22 +1,37 @@
 package org.bepass.oblivion;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SplashScreenActivity extends AppCompatActivity {
+@SuppressLint("CustomSplashScreen")
+public class SplashScreenActivity extends AppCompatActivity implements View.OnClickListener {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Initialize the LocaleHandler and set the locale
+        LocaleHandler localeHandler = new LocaleHandler(this);
         setContentView(R.layout.activity_splash_screen);
-        final int SPLASH_DISPLAY_LENGTH = 2750; // 2.75s
+        final int SHORT_SPLASH_DISPLAY_LENGTH = 1000; // 1 second
+        findViewById(R.id.splashScreen).setOnClickListener(this);
         new Handler().postDelayed(() -> {
             // Create an Intent that will start the Main Activity.
             Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
             SplashScreenActivity.this.startActivity(mainIntent);
             SplashScreenActivity.this.finish();
-        }, SPLASH_DISPLAY_LENGTH);
+        }, SHORT_SPLASH_DISPLAY_LENGTH);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // If the user clicks on the splash screen, move to the MainActivity immediately
+        Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+        SplashScreenActivity.this.startActivity(mainIntent);
+        SplashScreenActivity.this.finish();
     }
 }
