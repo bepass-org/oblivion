d60a324bfa555a81b402b6a78b1db35a6176933a
Benny <behnammnasehi@gmail.com>
2024-06-09 15:00:00 +0330

    Implemented ApplicationLoader class for global context access and initialization & added to manifest

---
+++ app/src/main/java/org/bepass/oblivion/base/ApplicationLoader.java
@@ -0,0 +1,37 @@
+package org.bepass.oblivion.base;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * ApplicationLoader is a custom Application class that extends the Android Application class.
+ * It is designed to provide a centralized context reference throughout the application.
+ */
+public class ApplicationLoader extends Application {
+
+    // Tag for logging purposes
+    private static final String TAG = "ApplicationLoader";
+
+    // Context reference
+    @SuppressLint("StaticFieldLeak")
+    private static Context context;
+
+    /**
+     * This method is called when the application is starting, before any activity, service, or receiver objects (excluding content providers) have been created.
+     * @see android.app.Application#onCreate()
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+
+    /**
+     * Returns the application context.
+     * @return The application context.
+     */
+    public static Context getAppCtx(){
+        return context;
+    }
+}
