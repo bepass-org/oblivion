addb3709645fadd6408c7576f35ebd443f02ae10
Benny <behnammnasehi@gmail.com>
2024-07-08 10:27:02 +0330

    add font both for en and fa lang create custom text view for handling font on configure change

---
+++ app/src/main/java/org/bepass/oblivion/component/OblivionTextView.java
@@ -0,0 +1,24 @@
+package org.bepass.oblivion.component;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class OblivionTextView extends AppCompatTextView {
+
+    public OblivionTextView(@NonNull Context context) {
+        super(context);
+    }
+
+    public OblivionTextView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public OblivionTextView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
